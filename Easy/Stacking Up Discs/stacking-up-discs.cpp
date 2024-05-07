e65d2a03ef1a2c59fbf5c2e44079360a874fd271
Antonio "Il Programmatore" D'Angelico <dangelico@libero.it>
2024-05-08 01:45:43 +0200

    Updated Solution - GfG to GitHub

--- Easy/Stacking Up Discs/stacking-up-discs.cpp
+++ Easy/Stacking Up Discs/stacking-up-discs.cpp
@@ -8,56 +8,26 @@ using namespace std;
 //User function template for C++
 
 class Solution{
-    vector<int> dp;
-    
-    int fun(int N,int R[],int H[], int prev, int i){
-        if (i == N)
-            return 0;
-        if (dp[i] > 0)
-            return dp[i];
-            
-            
-        int res, aux;
-        
-        res = 0;
-        for (int j = i;j < N;j++)
-        {
-            if (prev == -1)
-            {
-                aux = fun(N, R, H, j, j + 1) + H[j];
-                if (aux > res)
-                    res = aux;
-                continue;
-            }
-            if (R[j] > R[prev] && H[j] > H[prev])
-            {
-                aux = fun(N, R, H, j, j + 1) + H[j];
-                if (aux > res)
-                    res = aux;
-            }
-        }
-        dp[i] = res;
-        return res;
-    }
-    
     public:
     int StackingUpDiscs(int N,int R[],int H[]){
         //code here
         vector<pair<int,int>> D;
-        int R2[N], H2[N];
+        vector<int> dp;
 
         for (int i = 0;i < N;i++)
             D.push_back(make_pair(R[i], H[i]));
         sort(D.begin(), D.end());
         for (int i = 0;i < N;i++)
-        {
-            R2[i] = D[i].first;
-            H2[i] = D[i].second;
-        }
-        dp.resize(N, 0);
-        return fun(N, R2, H2, -1, 0);
+            dp.push_back(D[i].second);
+        for (int i = 1;i < N;i++)
+            for (int j = 0;j < i;j++)
+                if (D[j].first < D[i].first && D[j].second < D[i].second &&
+                    (dp[j] + D[i].second) > dp[i])
+                    dp[i] = dp[j] + D[i].second;
+        return *max_element(dp.begin(), dp.end());
     }
 };
+
 
 
 
