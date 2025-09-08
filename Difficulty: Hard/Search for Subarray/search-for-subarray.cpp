40bbcf695e8a6847d0e7c64a14d4da9ca3457f08
Antonio "Il Programmatore" D'Angelico <dangelico@libero.it>
2025-09-08 23:38:05 +0200

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Search for Subarray/search-for-subarray.cpp
@@ -0,0 +1,50 @@
+class Solution {
+  public:
+    vector<int> search(vector<int> &a, vector<int> &b) {
+        // code here
+        vector<int> pref, res;
+        int j;
+        
+        pref.resize(b.size());
+        j = 0;
+        pref[0] = 0;
+        for (int i = 1;i < (int)b.size();i++)
+        {
+            if (b[i] == b[j])
+            {
+                j++;
+                pref[i] = j;
+            }
+            else
+            {
+                if (j == 0)
+                    pref[i] == 0;
+                else
+                {
+                    j = pref[j - 1];
+                    i--;
+                }
+            }
+        }
+        j = 0;
+        for (int i = 0;i < (int)a.size();i++)
+        {
+            if (a[i] == b[j])
+            {
+                j++;
+                if (j == (int)b.size())
+                {
+                    res.push_back(i - j + 1);
+                    j = pref.back();
+                }
+            }
+            else
+                if (j > 0)
+                {
+                    j = pref[j - 1];
+                    i--;
+                }
+        }
+        return res;
+    }
+};
