983cb25749d1bd3d6c08172b5fe1e56eebbee596
Antonio "Il Programmatore" D'Angelico <dangelico@libero.it>
2025-10-06 23:23:16 +0200

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Search Pattern/search-pattern.cpp
+++ Difficulty: Hard/Search Pattern/search-pattern.cpp
@@ -6,37 +6,36 @@ class Solution {
         int i, j;
         
         pref_len.resize(pat.length(), 0);
-        i = 1;
         j = 0;
-        while (i < (int)pat.length())
+        for (i = 1;i < (int)pat.length();i++)
         {
             if (pat[i] == pat[j])
             {
                 j++;
                 pref_len[i] = j;
-                i++;
             }
             else
             {
                 if (j > 0)
+                {
                     j = pref_len[j - 1];
-                else
-                    i++;
+                    i--;
+                }
             }
         }
-        i = 0;
         j = 0;
+        i = 0;
         while (i < (int)txt.length())
         {
             if (pat[j] == txt[i])
             {
                 j++;
+                i++;
                 if (j == (int)pat.length())
                 {
-                    res.push_back(i - j + 1);
+                    res.push_back(i - j);
                     j = pref_len[j - 1];
                 }
-                i++;
             }
             else
             {
