1c878228728e995099fb7fb5d2ca0d3f6ead97a3
Antonio "Il Programmatore" D'Angelico <dangelico@libero.it>
2025-12-19 05:29:07 +0100

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Dictionary in Python - II/dictionary-in-python-ii.py
@@ -0,0 +1,7 @@
+def pair_sum(arr, sum):
+    # code here
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == sum:
+                return True
+    return False
