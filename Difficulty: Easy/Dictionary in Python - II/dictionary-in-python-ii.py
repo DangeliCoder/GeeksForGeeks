94651cd76ee72137bb256e48981cf590447bc370
Antonio "Il Programmatore" D'Angelico <dangelico@libero.it>
2025-12-20 03:37:41 +0100

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Dictionary in Python - II/dictionary-in-python-ii.py
+++ Difficulty: Easy/Dictionary in Python - II/dictionary-in-python-ii.py
@@ -1,7 +1,12 @@
 def pair_sum(arr, sum):
     # code here
+    add = {}
     for i in range(len(arr)):
-        for j in range(i + 1, len(arr)):
-            if arr[i] + arr[j] == sum:
+        add[sum - arr[i]] = arr[i]
+    for i in range(len(arr)):
+        if arr[i] == (sum / 2) and (sum % 2) == 0:
+            if (arr.count(arr[i]) > 1):
                 return True
+        elif arr[i] in add:
+            return True
     return False
